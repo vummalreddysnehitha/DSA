00776a29a2b5460c33552afaa8daa5993c93a54e
VUMMAL REDDY SNEHITHA REDDY <vummalreddysnehitha@gmail.com>
2026-07-25 10:31:02 +0530

    Time: 207 ms (87.22%) | Memory: 47 MB (41.35%) - LeetSync

---
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+
+        int[] pairXor = new int[2048];
+        int[] tripleXor = new int[2048];
+
+        // Compute all possible pair XORs
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                pairXor[nums[i] ^ nums[j]] = 1;
+            }
+        }
+
+        // Generate triplet XORs
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2048; j++) {
+                if (pairXor[j] == 1) {
+                    tripleXor[j ^ nums[i]] = 1;
+                }
+            }
+        }
+
+        // Count distinct XOR values
+        int cnt = 0;
+        for (int value : tripleXor) {
+            if (value == 1) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+}
